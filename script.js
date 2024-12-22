2bc04da906394e77cec82eca499d0ae5cc408e02
manueldesd <mdesouza.personal@gmail.com>
2024-12-23 00:02:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ document.addEventListener('DOMContentLoaded', () => {
   // Check if service worker is supported
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/service-worker.js')
+      navigator.serviceWorker.register('service-worker.js')
         .then((registration) => {
           console.log('Service Worker registered with scope:', registration.scope);
         })
