e63ad65a2f42af17765b823a501bb57ffdaaa3dc
Manueldsza <mdesouza.personal@gmail.com>
2024-12-25 21:12:25 +0200

    update index.html, mdsd.css, script.js new menue and heading styles

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-// Detect and apply theme preference
-const darkModeToggle = document.getElementById('darkModeToggle');
-
-// Check if dark mode is already enabled by the user
-if (localStorage.getItem('darkMode') === 'enabled') {
-    document.body.classList.add('dark-mode');
-    darkModeToggle.checked = true;
-}
-
-// Toggle dark mode
-darkModeToggle.addEventListener('change', function() {
-    if (this.checked) {
-        document.body.classList.add('dark-mode');
-        localStorage.setItem('darkMode', 'enabled');
-    } else {
-        document.body.classList.remove('dark-mode');
-        localStorage.setItem('darkMode', 'disabled');
-    }
-});
-
 function toggleMenu() {
     const menu = document.getElementById('navMenu');
     const hamburgerIcon = document.querySelector('.hamburger-icon');
