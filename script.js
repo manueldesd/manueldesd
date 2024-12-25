1c22af72e042f6bebd596568324bc4e1dde51a79
Manueldsza <mdesouza.personal@gmail.com>
2024-12-25 20:13:36 +0200

    Added hamburger bar js

--- script.js
+++ script.js
@@ -17,3 +17,12 @@ darkModeToggle.addEventListener('change', function() {
         localStorage.setItem('darkMode', 'disabled');
     }
 });
+
+function toggleMenu() {
+    const menu = document.getElementById('navMenu');
+    const hamburgerIcon = document.querySelector('.hamburger-icon');
+
+    // Toggle visibility of the navigation menu
+    menu.classList.toggle('visible');
+    hamburgerIcon.classList.toggle('open');
+}
