3c5a0d8ad7d97acdc1d7912f0ac91797187b9d5b
Manueldsza <mdesouza.personal@gmail.com>
2024-12-30 10:24:56 +0200

    Hi, update

--- script_2.js
+++ script_2.js
@@ -8,3 +8,24 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
   });
   
+  // Function to generate a random color in HEX format
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+  
+  // Function to change the background color of h1 every few seconds
+  function changeBackgroundColor() {
+    const h1 = document.querySelector('h1');
+    setInterval(() => {
+      h1.style.backgroundColor = getRandomColor(); // Set random background color
+    }, 3000); // Change color every 3 seconds (3000ms)
+  }
+  
+  // Call the function to start changing the color
+  changeBackgroundColor();
+  
