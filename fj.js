61747bbddd4fe187859cc3ab823da9f8f22da201
manueldesd <mdesouza.personal@gmail.com>
2025-01-08 22:27:15 +0200

    update fj.js

--- fj.js
+++ fj.js
@@ -188,3 +188,15 @@ document.addEventListener("DOMContentLoaded", () => {
 
   calculateTotals();
 });
+
+function toggleMenu() {
+
+    const menu = document.getElementById('navMenu');
+
+    const hamburgerIcon = document.querySelector('.hamburger-icon');
+
+    // Toggle visibility of the navigation menu
+    menu.classList.toggle('visible');
+    hamburgerIcon.classList.toggle('open');
+}
+
