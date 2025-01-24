793f4ebf7541c7b5b4dd1ec8984221d534495e1b
manueldesd <mdesouza.personal@gmail.com>
2025-01-24 21:46:51 +0200

    Update fj1.js

--- fj1.js
+++ fj1.js
@@ -26,7 +26,6 @@ function releaseWakeLock() {
 window.addEventListener('load', requestWakeLock);
 window.addEventListener('beforeunload', releaseWakeLock);
 
-// Calculate the total cash based on denominations and input values
 function calculateTotal() {
     // Denomination and input mapping
     const values = {
@@ -116,18 +115,5 @@ function clearInputs() {
     localStorage.removeItem('grandTotal');
 }
 
-// Add event listener to select all text when input is clicked
-function addInputEventListeners() {
-    for (let i = 1; i <= 11; i++) {
-        const inputField = document.getElementById(`input${i}`);
-        inputField.addEventListener('click', () => {
-            inputField.select(); // Select all text when clicked
-        });
-    }
-}
-
-// Restore inputs and set event listeners on page load
-window.onload = () => {
-    restoreInputs();
-    addInputEventListeners();
-};
+// Restore inputs on page load
+window.onload = restoreInputs;
